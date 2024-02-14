ef465d9c81d5c93dd2d0f8feef499b790dd2aa03
Godlearningtocode <shivam.ayush.agarwal@gmail.com>
2024-02-15 00:34:01 +0530

    project complete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { rulesToMonitor } = require("nodemon/lib/monitor/match");
+
 function Node(data, left, right) {
     return {
         data: data,
@@ -75,21 +77,116 @@ function Tree(array) {
         return node
     }
 
+    //function levelOrder using a loop
+    function levelOrder(callback) {
+        let node = root;
+        let stack = [node];
+        let traversed = []
+
+        if(node == null) return node;
+
+        while(node) {
+            traversed.push(node.data)
+            if(node.left) stack.push(node.left);
+            if(node.right) stack.push(node.right);
+            if(callback) callback(node)
+            node = stack[1]
+            stack.shift();
+        }
+
+        if(!callback) return traversed;
+    }
+
+    function inOrder(node = this.root, callback, result = []) {
+        if(!node) return []
+        if(node == null) return
+        inOrder(node.left, callback, result);
+        result.push(node.data)
+        inOrder(node.right, callback, result)
+        if(result) return result 
+    }
+
+    function preOrder(node = this.root, callback, result = []) {
+        if(!node) return result;
+        if(node == null) return
+        result.push(node.data);
+        preOrder(node.left, callback, result);
+        preOrder(node.right, callback, result);
+        return result
+    }
+
+    function postOrder(node = this.root, callback, result = []) {
+        if(!node) return result;
+        if(node == null) return
+
+        postOrder(node.left, callback, result);
+        postOrder(node.right, callback, result);
+        callback ? callback(node) : result.push(node.data)
+
+        return result;
+    }
+
+    function height(root = this.root) {
+        if(root == null) return -1;
+        let left = height(root.left);
+        let right = height(root.right)
+        return Math.max(left, right) + 1
+    }
+
+    function depth(node, root = this.root) {
+        if(root == null) return 
+
+        let distance = -1
+
+        if(root.data === node) return distance += 1;
+        if((distance = depth(node, root.left)) >= 0) return distance += 1;
+        if((distance = depth(node, root.right)) >= 0) return distance += 1;
+
+        return distance
+    }
+
+    function isBalanced(node = this.root) {
+        if(node == null) return true;
+        let left = height(node.left);
+        let right = height(node.right);
+        let heightDifference = Math .abs(left - right);
+
+        return (heightDifference && isBalanced(node.left) && isBalanced(node.right))
+    }
+
+    function rebalance() {
+        if(isBalanced(this.root)) return true
+        const data = [...new Set(this.inOrder(el => el, root))]
+        root = this.buildTree(data, 0, data.length - 1)
+        return root
+    }
+
     return {
-        buildTree, root, sorted, insert, deleteValue, find
+        buildTree, root, sorted, insert, deleteValue, find, levelOrder, inOrder, preOrder, postOrder, height, depth, isBalanced, rebalance
     }
 }
 
-let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+let script = []
+function fillScript(script) {
+    for(let i = 0; i < 10; i++) {
+        script.push(Math.floor((Math.random() * 1000) + 1))
+    }
+
+    return script
+}
+
+let test = new Tree(fillScript(script))
 
 console.log(test.root);
 console.log(test.sorted);
-console.log(test.insert(0));
-console.log(test.deleteValue(0));
-console.log(test.insert(2));
-console.log(test.deleteValue(3));
-console.log(test.deleteValue(1));
-console.log(test.insert(0));
-console.log(test.deleteValue(4));
-console.log(test.deleteValue(5));
-console.log(test.find(6345))
+console.log(test.levelOrder());
+console.log(test.inOrder());
+console.log(test.preOrder());
+console.log(test.postOrder());
+console.log(test.height());
+console.log(test.depth())
+console.log(test.isBalanced());
+console.log(test.rebalance())
+console.log(test.inOrder());
+console.log(test.preOrder());
+console.log(test.postOrder());
